1351943158df391fa23aab5b29b2211edae58c2c
Arina Zubova <arina777pototskaya@gmail.com>
2022-08-16 18:12:31 +0300

    fix linter comments

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -18,12 +18,9 @@ def generate_diff(file1, file2):
                 diff += f'{i}: {source1[i]}\n'
             else:
                 diff += f'-{i}: {source1[i]}\n+{i}: {source2[i]}\n'
-        elif i in source1 and not i in source2:
+        elif i in source1 and i not in source2:
             diff += f'-{i}: {source1[i]}\n'
-        elif not i in source1 and i in source2:
+        elif i not in source1 and i in source2:
             diff += f'+{i}: {source2[i]}\n'
 
     return f'{{\n{diff}}}'
-
-
-
