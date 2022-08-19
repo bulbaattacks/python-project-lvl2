0b7525b32a950f1e7edeb088c17ab118612b4085
Arina Zubova <arina777pototskaya@gmail.com>
2022-08-19 12:35:20 +0300

    update tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,6 +2,6 @@ from gendiff import generate_diff
 
 
 def test_generate_diff():
-    with open('/Users/arinazubova/super_hexlet/python-project-lvl2/tests/fixtures/result.txt', 'r') as file:
+    with open('tests/fixtures/result.txt', 'r') as file:
         f = file.read()
-    assert generate_diff('/Users/arinazubova/super_hexlet/python-project-lvl2/tests/fixtures/file1.json', '/Users/arinazubova/super_hexlet/python-project-lvl2/tests/fixtures/file2.json') == f
+    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == f
