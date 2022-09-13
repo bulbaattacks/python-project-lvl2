0540ab071d9ddcf3c02aa273a7109962b9874696
Arina Zubova <arina777pototskaya@gmail.com>
2022-09-13 18:11:41 +0300

    refactor test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -15,6 +15,7 @@ def test_generate_diff_plain():
         f = file.read()
     assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain') == f
 
+
 def test_generate_diff_json():
     """test two not empty files with plain format"""
     with open('tests/fixtures/result3.txt', 'r') as file:
@@ -26,11 +27,3 @@ def test_generate_diff_wrong_format():
     """test two not empty files"""
     with pytest.raises(Exception):
         generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file3.wrong')
-
-
-
-
-
-
-
-
