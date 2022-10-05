f9a3ac39212e836949bcb278eb4e501e4bd146c0
Arina Zubova <arina777pototskaya@gmail.com>
2022-10-05 15:34:28 +0300

    refactor json.py

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,9 +1,5 @@
 import json
 
 
-def json_maker(diff_dict):
-    return json.dumps(diff_dict, sort_keys=True, indent=1)
-
-
 def format_json(diff_dict):
-    return json_maker(diff_dict)
+    return json.dumps(diff_dict, sort_keys=True, indent=1)
