2b412475d26d172b56f43c9693cd3afe25dd661e
Arina Zubova <arina777pototskaya@gmail.com>
2022-10-05 17:37:46 +0300

    refactor test_gendiff.py: add fixture_path_constructor function

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,14 +1,20 @@
 from gendiff import generate_diff
 import pytest
+import os
+
+
+def fixture_path_constructor(file_name):
+    return os.path.join("tests", "fixtures", file_name)
 
 
 @pytest.mark.parametrize("format,expected",
-                         [("stylish", "tests/fixtures/result1.txt"),
-                          ("plain", "tests/fixtures/result2.txt"),
-                          ("json", "tests/fixtures/result3.txt")])
+                         [("stylish", "result1.txt"),
+                          ("plain", "result2.txt"),
+                          ("json", "result3.txt")])
 def test_generate_diff(format, expected):
     """test two not empty files"""
-    with open(expected, 'r') as file:
+    full_file_name = fixture_path_constructor(expected)
+    with open(full_file_name, 'r') as file:
         f = file.read()
         assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', format) == f
 
