420c62464d406c12d38ad9bca1e16acfdeb99be2
Arina Zubova <arina777pototskaya@gmail.com>
2022-10-06 15:38:41 +0300

    refactor test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -15,8 +15,10 @@ def test_generate_diff(format, expected):
     """test two not empty files"""
     full_file_name = generate_fixture_path(expected)
     with open(full_file_name, 'r') as file:
-        f = file.read()
-        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', format) == f
+        result = file.read()
+        path_to_file1 = generate_fixture_path('file1.json')
+        path_to_file2 = generate_fixture_path('file2.json')
+        assert generate_diff(path_to_file1, path_to_file2, format) == result
 
 
 def test_generate_diff_wrong_format():
