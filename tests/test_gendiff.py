56baecafac6df34e0ff79b9c6856eb70ba239cef
Arina Zubova <arina777pototskaya@gmail.com>
2022-10-06 17:17:16 +0300

    refactor test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,21 +7,25 @@ def generate_fixture_path(file_name):
     return os.path.join("tests", "fixtures", file_name)
 
 
-@pytest.mark.parametrize("format,expected",
-                         [("stylish", "result1.txt"),
-                          ("plain", "result2.txt"),
-                          ("json", "result3.txt")])
-def test_generate_diff(format, expected):
+@pytest.mark.parametrize("file1, file2, format, expected",
+                         [("file1.json", "file2.json", "stylish", "result1.txt"),
+                          ("file1.json", "file2.json", "plain", "result2.txt"),
+                          ("file1.json", "file2.json", "json", "result3.txt")])
+def test_generate_diff(file1, file2, format, expected):
     """test two not empty files"""
     full_file_name = generate_fixture_path(expected)
+    path_to_file1 = generate_fixture_path(file1)
+    path_to_file2 = generate_fixture_path(file2)
     with open(full_file_name, 'r') as file:
         result = file.read()
-        path_to_file1 = generate_fixture_path('file1.json')
-        path_to_file2 = generate_fixture_path('file2.json')
         assert generate_diff(path_to_file1, path_to_file2, format) == result
 
 
-def test_generate_diff_wrong_format():
+@pytest.mark.parametrize("right_format_file, wrong_format_file",
+                         [("file1.json", "file2.wrong")])
+def test_generate_diff_wrong_format(right_format_file, wrong_format_file):
     """test two not empty files with wrong_format of a file"""
+    path_to_right_format_file = generate_fixture_path(right_format_file)
+    path_to_wrong_format_file = generate_fixture_path(wrong_format_file)
     with pytest.raises(Exception):
-        generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file3.wrong')
+        generate_diff(path_to_right_format_file, path_to_wrong_format_file)
